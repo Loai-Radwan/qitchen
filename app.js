b86507467392ab76f2f37fd5e1e311a0c2578712
LoaiRadwan <loairadwan6@gmail.com>
2025-08-31 20:24:36 +0100

    add padding to header

--- app.js
+++ app.js
@@ -1,125 +1,116 @@
+let menu = document.querySelector(".hidden-nav");
+let menuButton = document.querySelector(".menu-icons");
 
+let mainLinks = document.querySelectorAll(".main li");
 
-let menu = document.querySelector('.hidden-nav')
-let menuButton = document.querySelector('.menu-icons')
+let menuSection = document.querySelector(".menu .menu-section");
+let makiFood = document.getElementById("maki");
+let uramakiFood = document.getElementById("uramaki");
+let specialFood = document.getElementById("special");
 
-let mainLinks = document.querySelectorAll('.main li')
+let copyRight = document.querySelector("footer p span");
 
-let menuSection = document.querySelector('.menu .menu-section')
-let makiFood = document.getElementById('maki')
-let uramakiFood = document.getElementById('uramaki')
-let specialFood = document.getElementById('special')
-
-let copyRight = document.querySelector('footer p span')
-
-let images = document.querySelectorAll('img')
-Array.from(images).forEach(img => {
-    img.setAttribute('loading', 'lazy')
-})
+let images = document.querySelectorAll("img");
+Array.from(images).forEach((img) => {
+    img.setAttribute("loading", "lazy");
+});
 
 console.log(images);
 
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("active");
+    menu.classList.toggle("show");
+});
 
-
-menuButton.addEventListener('click' , () => {
-    menuButton.classList.toggle('active')
-    menu.classList.toggle('show')
-})
-
-Array.from(mainLinks).forEach(link => {
-    link.addEventListener('click' , () => {
+Array.from(mainLinks).forEach((link) => {
+    link.addEventListener("click", () => {
         // link.classList.add('active')
-            location.href = `https://loai-radwan.github.io/qitchen/${link.dataset.link}`
-            console.log(link.dataset.link)
-            // link.classList.remove('active')
+        location.href = `https://loai-radwan.github.io/qitchen/${link.dataset.link}`;
+        console.log(link.dataset.link);
+        // link.classList.remove('active')
+    });
+});
 
-    })
-})
-
-let foodHttp = new XMLHttpRequest()
-foodHttp.onreadystatechange = function() {
+let foodHttp = new XMLHttpRequest();
+foodHttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        let food = JSON.parse(foodHttp.responseText)
+        let food = JSON.parse(foodHttp.responseText);
 
-        food.maki.forEach(maki => {
-            let box = document.createElement('div')
-            box.classList.add('food-box')
-            let img = document.createElement('img')
-            img.setAttribute('src',maki.image)
-            img.setAttribute('alt' , maki.alt)
+        food.maki.forEach((maki) => {
+            let box = document.createElement("div");
+            box.classList.add("food-box");
+            let img = document.createElement("img");
+            img.setAttribute("src", maki.image);
+            img.setAttribute("alt", maki.alt);
 
-            let text = document.createElement('div')
-            text.classList.add('text')
-            
-            let heading = document.createElement('h4')
-            heading.innerHTML = `<span>${maki.title} </span>  <span> ${maki.price} </span>  `
+            let text = document.createElement("div");
+            text.classList.add("text");
 
-            let description = document.createElement('p')
-            description.textContent = maki.description
+            let heading = document.createElement("h4");
+            heading.innerHTML = `<span>${maki.title} </span>  <span> ${maki.price} </span>  `;
 
-            text.appendChild(heading)
-            text.appendChild(description)
+            let description = document.createElement("p");
+            description.textContent = maki.description;
 
-            box.appendChild(img)
-            box.appendChild(text)
+            text.appendChild(heading);
+            text.appendChild(description);
 
-            makiFood.appendChild(box)
-        })
-        food.uramaki.forEach(uramaki => {
-            let box = document.createElement('div')
-            box.classList.add('food-box')
-            let img = document.createElement('img')
-            img.setAttribute('src',uramaki.image)
-            img.setAttribute('alt' , uramaki.alt)
+            box.appendChild(img);
+            box.appendChild(text);
 
-            let text = document.createElement('div')
-            text.classList.add('text')
-            
-            let heading = document.createElement('h4')
-            heading.innerHTML = `<span>${uramaki.title} </span>  <span> ${uramaki.price} </span>  `
+            makiFood.appendChild(box);
+        });
+        food.uramaki.forEach((uramaki) => {
+            let box = document.createElement("div");
+            box.classList.add("food-box");
+            let img = document.createElement("img");
+            img.setAttribute("src", uramaki.image);
+            img.setAttribute("alt", uramaki.alt);
 
-            let description = document.createElement('p')
-            description.textContent = uramaki.description
+            let text = document.createElement("div");
+            text.classList.add("text");
 
-            text.appendChild(heading)
-            text.appendChild(description)
+            let heading = document.createElement("h4");
+            heading.innerHTML = `<span>${uramaki.title} </span>  <span> ${uramaki.price} </span>  `;
 
-            box.appendChild(img)
-            box.appendChild(text)
+            let description = document.createElement("p");
+            description.textContent = uramaki.description;
 
-            uramakiFood.appendChild(box)
-        })
-        food.special.forEach(special => {
-            let box = document.createElement('div')
-            box.classList.add('food-box')
-            let img = document.createElement('img')
-            img.setAttribute('src',special.image)
-            img.setAttribute('alt' , special.alt)
+            text.appendChild(heading);
+            text.appendChild(description);
 
-            let text = document.createElement('div')
-            text.classList.add('text')
-            
-            let heading = document.createElement('h4')
-            heading.innerHTML = `<span>${special.title} </span>  <span> ${special.price} </span>  `
+            box.appendChild(img);
+            box.appendChild(text);
 
-            let description = document.createElement('p')
-            description.textContent = special.description
+            uramakiFood.appendChild(box);
+        });
+        food.special.forEach((special) => {
+            let box = document.createElement("div");
+            box.classList.add("food-box");
+            let img = document.createElement("img");
+            img.setAttribute("src", special.image);
+            img.setAttribute("alt", special.alt);
 
-            text.appendChild(heading)
-            text.appendChild(description)
+            let text = document.createElement("div");
+            text.classList.add("text");
 
-            box.appendChild(img)
-            box.appendChild(text)
+            let heading = document.createElement("h4");
+            heading.innerHTML = `<span>${special.title} </span>  <span> ${special.price} </span>  `;
 
-            specialFood.appendChild(box)
-        })
+            let description = document.createElement("p");
+            description.textContent = special.description;
 
+            text.appendChild(heading);
+            text.appendChild(description);
 
+            box.appendChild(img);
+            box.appendChild(text);
+
+            specialFood.appendChild(box);
+        });
     }
 };
-foodHttp.open('GET' , 'js/data.json' , true)
-foodHttp.send()
+foodHttp.open("GET", "js/data.json", true);
+foodHttp.send();
 
-
-
-copyRight.innerHTML = new Date().getFullYear()
+copyRight.innerHTML = new Date().getFullYear();
